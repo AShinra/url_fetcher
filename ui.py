8f32b2742a82adac4ac252c957e2534dafd87614
Athan <jonpuray@gmail.com>
2025-09-03 14:08:51 +0800

    updated stored configs to mongodb

--- ui.py
+++ ui.py
@@ -3,15 +3,15 @@ import io
 import contextlib
 from PySide6.QtWidgets import (
     QApplication, QMainWindow, QWidget, QLabel, QPushButton,
-    QVBoxLayout, QComboBox, QPlainTextEdit, QHBoxLayout, QMessageBox, QStackedWidget, QDateEdit, QSizePolicy, QLineEdit
+    QVBoxLayout, QComboBox, QPlainTextEdit, QHBoxLayout, QMessageBox, QStackedWidget, QDateEdit, QSizePolicy, QLineEdit, QTextEdit, QGridLayout
 )
-from PySide6.QtCore import QDate
+from PySide6.QtCore import Qt, QDate
 from pymongo import MongoClient
 from datetime import datetime
 import pandas as pd
 import json
-from url_fetcher1 import fetcher1
-from url_fetcher2 import fetcher2
+from url_fetcher import fetcher
+from common import connect_to_mongodb
 
 
 
@@ -21,6 +21,10 @@ class SimpleUI(QMainWindow):
         self.setWindowTitle("URL Fetcher")
         self.setGeometry(100, 100, 300, 300)
 
+        # initialize db
+        self.client = connect_to_mongodb()
+        self.db = self.client["fetcher"]
+
         # --- Central widget (QStackedWidget to switch pages) ---
         self.stacked_widget = QStackedWidget()
         self.setCentralWidget(self.stacked_widget)
@@ -29,17 +33,20 @@ class SimpleUI(QMainWindow):
         self.fetcher_page = QWidget()
         self.dashboard_page = QWidget()
         self.mongodb_page = QWidget()
-        self.settings_page = QWidget()
+        self.edit_settings_page = QWidget()
+        self.add_settings_page = QWidget()
 
         self.init_fetcher_page()
         self.init_dashboard_page()
         self.init_mongodb_page()
-        self.init_settings_page()
+        self.init_edit_settings_page()
+        self.init_add_settings_page()
 
         self.stacked_widget.addWidget(self.fetcher_page)
         self.stacked_widget.addWidget(self.dashboard_page)
         self.stacked_widget.addWidget(self.mongodb_page)
-        self.stacked_widget.addWidget(self.settings_page)
+        self.stacked_widget.addWidget(self.edit_settings_page)
+        self.stacked_widget.addWidget(self.add_settings_page)
 
         # --- Menu Bar ---
         menubar = self.menuBar()
@@ -61,8 +68,14 @@ class SimpleUI(QMainWindow):
 
         # Settings Menu
         settings_menu = menubar.addMenu("Settings")
-        open_settings_action = settings_menu.addAction("Config")
-        open_settings_action.triggered.connect(lambda: self.stacked_widget.setCurrentWidget(self.settings_page))
+        open_add_settings_action = settings_menu.addAction('Add')
+        open_add_settings_action.triggered.connect(lambda: self.stacked_widget.setCurrentWidget(self.add_settings_page))
+        open_edit_settings_action = settings_menu.addAction('Edit')
+        open_edit_settings_action.triggered.connect(lambda: self.stacked_widget.setCurrentWidget(self.edit_settings_page))
+
+        # open_settings_action = settings_menu.addAction("Edit")
+
+        # open_settings_action.triggered.connect(lambda: self.stacked_widget.setCurrentWidget(self.settings_page))
 
         # Help Menu
         help_menu = menubar.addMenu("Help")
@@ -135,14 +148,11 @@ class SimpleUI(QMainWindow):
         buffer = io.StringIO()
         with contextlib.redirect_stdout(buffer), contextlib.redirect_stderr(buffer):
             fetcher_data = data[fetcher_setting[selected_item][0]]
+            fetcher_number = fetcher_setting[selected_item][-1]
+            fetcher(fetcher_data, fetcher_number)
             
-            if fetcher_setting[selected_item][-1]==1:
-                fetcher1(fetcher_data)
-            if fetcher_setting[selected_item][-1]==2:
-                fetcher2(fetcher_data)
-        
-        logs = buffer.getvalue()
-        self.console_fetcher.appendPlainText(logs if logs else "No logs captured.")
+            logs = buffer.getvalue()
+            self.console_fetcher.appendPlainText(logs if logs else "No logs captured.")
         # else:
         #     self.console_fetcher.appendPlainText("Processing other sources not yet implemented.")
 
@@ -307,14 +317,236 @@ class SimpleUI(QMainWindow):
 
     # ----- Settings Area -----
 
-    def init_settings_page(self):
+    def init_edit_settings_page(self):
 
-        # 
+        # self.client = connect_to_mongodb()
+        # self.db = self.client["fetcher"]
+        collection = self.db["publication_settings"]
+        items = list(collection.find())
 
+        # get the publication list
+        pub_list = [item['pub_name'] for item in items]
+        
+        # create widgets
+        self.pub_label = QLabel('Publication', self,) #label for publication combobox
+        self.pub_label.setFixedWidth(100)
+        
+        self.pub_combo = QComboBox(self) #create publication combobox
+        self.pub_combo.setFixedWidth(200)
+        self.pub_combo.setStyleSheet("border: 2px solid orange;")
+        self.pub_combo.addItems(pub_list) #add publications to conbobox options
+
+        self.select_button = QPushButton('Select', self) #create select button
+        self.select_button.setFixedWidth(200)
+
+        self.update_button = QPushButton('Update', self) #create select button
+        self.update_button.setFixedWidth(200)
+
+        self.label_url = QLabel('URL', self)
+        self.textbox_url = QLineEdit()
+
+        self.label_container = QLabel('Container Selector', self)
+        self.textbox_container = QLineEdit()
+
+        self.label_title = QLabel('Title Selector', self)
+        self.textbox_title = QLineEdit()
+
+        self.label_fetcherno = QLabel('Fetcher', self)
+        self.textbox_fetcherno = QLineEdit()
+        self.textbox_fetcherno.setFixedWidth(30)
+
+        right_layout = QVBoxLayout()
+        left_layout = QVBoxLayout()
+        bottom_layout = QHBoxLayout()      
+        bl_layout = QVBoxLayout()
+        br_layout = QVBoxLayout()
+        top_layout = QHBoxLayout()
+        layout = QVBoxLayout()
+
+        left_layout.addWidget(self.pub_label)
+        left_layout.addStretch()
+
+        right_layout.addWidget(self.pub_combo)
+        right_layout.addWidget(self.select_button)
+        right_layout.addStretch()
+
+        bl_layout.addWidget(self.label_url)
+        bl_layout.addWidget(self.label_container)
+        bl_layout.addWidget(self.label_title)
+        bl_layout.addWidget(self.label_fetcherno)
+
+        br_layout.addWidget(self.textbox_url)
+        br_layout.addWidget(self.textbox_container)
+        br_layout.addWidget(self.textbox_title)
+        br_layout.addWidget(self.textbox_fetcherno)
+        br_layout.addWidget(self.update_button)
+
+        bottom_layout.addLayout(bl_layout)
+        bottom_layout.addLayout(br_layout)
+
+        top_layout.addLayout(left_layout)
+        top_layout.addLayout(right_layout)
+        top_layout.addStretch()
+
+        layout.addLayout(top_layout)
+        layout.addLayout(bottom_layout)
+        layout.addStretch()
+
+        self.edit_settings_page.setLayout(layout)
+
+        # button behavior
+        self.select_button.clicked.connect(self.select_publication_setting)
+        self.update_button.clicked.connect(self.update_publication_setting)
 
         return
-
     
+    def select_publication_setting(self):
+        """Load selected settings"""
+        collection = self.db['publication_settings']
+        docs = list(collection.find({'pub_name':self.pub_combo.currentText()}))
+        self.textbox_url.setText(docs[0]['fetcher_url'])
+        self.textbox_container.setText(docs[0]['selector_container'])
+        self.textbox_title.setText(docs[0]['selector_title'])
+        self.textbox_fetcherno.setText(str(docs[0]['fetcher_no']))
+
+    def update_publication_setting(self):
+        """Update publication setting"""
+        url = self.textbox_url.text()
+        container = self.textbox_container.text()
+        title = self.textbox_title.text()
+        fetcherno = int(self.textbox_fetcherno.text())
+        
+        collection = self.db['publication_settings']
+        query_text = self.pub_combo.currentText()
+        query = {'pub_name':query_text}
+
+        new_values = {'$set':{
+            'fetcher_url':url,
+            'selector_container':container,
+            'selector_title':title,
+            'fetcher_no':fetcherno
+        }}
+
+        result = collection.update_one(query, new_values)
+        
+        if result.modified_count > 0:
+            QMessageBox.information(self, "Success", "Document updated successfully!")
+        elif result.matched_count > 0:
+            QMessageBox.warning(self, "No Change", "Document found, but no changes were made.")
+        else:
+            QMessageBox.critical(self, "Error", "No matching document found.")
+
+    def init_add_settings_page(self):
+        # create widgets
+        # ---Labels---
+        label_pub = QLabel('PUBLICATION', self)
+        label_pub.setAlignment(Qt.AlignCenter)
+        label_pub.setStyleSheet("border: 2px solid gray; color: cyan; font-weight: bold")
+        label_pub.setFixedWidth(150)
+
+        label_url = QLabel('URL', self)
+        label_url.setAlignment(Qt.AlignCenter)
+        label_url.setStyleSheet("border: 2px solid gray; color: cyan; font-weight: bold")
+        label_url.setFixedWidth(150)
+
+        label_selectors = QLabel('SELECTORS', self)
+        label_selectors.setAlignment(Qt.AlignCenter)
+        label_selectors.setStyleSheet("border: 2px solid gray; color: cyan; font-weight: bold")
+        label_selectors.setFixedWidth(150)
+
+        label_container = QLabel('CONTAINER', self)
+        label_container.setAlignment(Qt.AlignCenter)
+        label_container.setStyleSheet("border: 2px solid gray; color: cyan; font-weight: bold")
+        label_container.setFixedWidth(150)
+
+        label_title = QLabel('TITLE', self)
+        label_title.setAlignment(Qt.AlignCenter)
+        label_title.setStyleSheet("border: 2px solid gray; color: cyan; font-weight: bold")
+        label_title.setFixedWidth(150)
+
+        label_fetcherno = QLabel('FETCHER NO.', self)
+        label_fetcherno.setAlignment(Qt.AlignCenter)
+        label_fetcherno.setStyleSheet("border: 2px solid gray; color: cyan; font-weight: bold")
+        label_fetcherno.setFixedWidth(150)
+
+        # ---Textboxes---
+        self.tb_pub = QLineEdit()
+        self.tb_url = QLineEdit()
+        self.tb_container = QLineEdit()
+        self.tb_title = QLineEdit()
+        self.combo_fetcherno = QComboBox(self)
+        self.combo_fetcherno.setFixedWidth(40)
+        self.combo_fetcherno.addItems([str(i) for i in range(1, 11)])
+        # ---Buttons---
+        btn_add = QPushButton('ADD CONFIGURATION', self)
+        btn_clear = QPushButton('CLEAR', self)
+
+        # create layout containers
+        main_layout = QVBoxLayout()
+        container_layout = QGridLayout()
+        bottom_layout = QHBoxLayout()
+        
+        # add widgets to layout
+        container_layout.addWidget(label_pub, 0, 0)
+        container_layout.addWidget(self.tb_pub, 0, 1)
+        container_layout.addWidget(label_url, 1, 0)
+        container_layout.addWidget(self.tb_url, 1, 1)
+        container_layout.addWidget(label_selectors, 2, 0)
+        container_layout.addWidget(label_container, 3, 0)
+        container_layout.addWidget(self.tb_container, 3, 1)
+        container_layout.addWidget(label_title, 4, 0)
+        container_layout.addWidget(self.tb_title, 4, 1)
+        container_layout.addWidget(label_fetcherno, 5, 0)
+        container_layout.addWidget(self.combo_fetcherno, 5, 1)
+
+        bottom_layout.addWidget(btn_add)
+        bottom_layout.addWidget(btn_clear)
+        # container_layout.addWidget(btn_add, 6, 1)
+        # container_layout.addWidget(btn_clear, 7, 1)
+        
+        main_layout.addLayout(container_layout)
+        main_layout.addLayout(bottom_layout)
+        main_layout.addStretch()
+        
+        self.add_settings_page.setLayout(main_layout)
+
+        btn_add.clicked.connect(self.add_configuration)
+        btn_clear.clicked.connect(self.clear_fields)
+    
+    def add_configuration(self):
+        """Add new configuration"""
+        pub = self.tb_pub.text()
+        url = self.tb_url.text()
+        container = self.tb_container.text()
+        title = self.tb_title.text()
+        fetcherno = int(self.combo_fetcherno.currentText())
+
+        if pub=='' or url=='' or container=='' or title=='':
+            QMessageBox.critical(self, "Error", "Some fields are blank")
+        else:
+            collection = self.db['publication_settings']
+
+            document = {
+                'fetcher_no':fetcherno,
+                'fetcher_url':url,
+                'pub_name':pub,
+                'selector_container':container,
+                'selector_title':title
+            }
+
+            if not collection.find_one({"pub_name": pub}):
+                result = collection.insert_one(document)
+                QMessageBox.information(self, "Success", "New configuration added")                
+            else:
+                QMessageBox.warning(self, "Existing", f"There is an existing configuration for {pub}")
+
+
+    def clear_fields(self):
+        """clear all fields"""
+        self.tb_pub.clear()
+        self.tb_container.clear()
+        self.tb_title.clear()
+        self.tb_url.clear()
 
     def close_app(self):
         """Exit the application."""
--- url_fetcher.py
+++ url_fetcher.py
@@ -3,9 +3,10 @@ from common import connect_to_mongodb, insert_document, check_if_document_exist,
 import re
 from datetime import datetime
 import json
+import time
 
 
-def url_fetcher(fetcher_url, fetcher_title_selector, fetcher_url_selector):
+def url_fetcher1(fetcher_url, fetcher_title_selector, fetcher_url_selector):
     url_list = []
     with sync_playwright() as p:
         browser = p.chromium.launch(headless=True)
@@ -30,15 +31,50 @@ def url_fetcher(fetcher_url, fetcher_title_selector, fetcher_url_selector):
                     
         browser.close()
     return url_list
+
+
+def url_fetcher2(fetcher_url, fetcher_title_selector, fetcher_url_selector):
+    url_list = []
+    with sync_playwright() as p:
+        browser = p.chromium.launch(headless=True)
+        page = browser.new_page()
+        response = get_url_response(page, f'{fetcher_url}')
+
+        for i in range(1, 21):
+            page.keyboard.press("End")
+            time.sleep(5)
+
+        if response.status == 200:
+            try:
+                page.wait_for_selector(fetcher_title_selector, timeout=60000)  # wait for the element to load
+                items = page.query_selector_all(fetcher_title_selector)                
+            except Exception as e:
+                print(f'page {i} - {e}')
+            else:
+                for item in items:
+                    # get url
+                    try:
+                        url = item.query_selector(fetcher_url_selector).query_selector('a').get_attribute('href')
+                    except:
+                        url = item.query_selector('a').get_attribute('href')
+                    finally:
+                        url_list.append([datetime.now(), url])
+                    
+        browser.close()
+    return url_list
     
 
-def fetcher1(fetcher_data):
+def fetcher(fetcher_data, fetcher_number):
 
     fetcher_url = fetcher_data['url']
     fetcher_title_selector = fetcher_data['title_selector']
     fetcher_url_selector = fetcher_data['url_selector']
 
-    url_list = url_fetcher(fetcher_url, fetcher_title_selector, fetcher_url_selector)
+    if fetcher_number==1:
+        url_list = url_fetcher1(fetcher_url, fetcher_title_selector, fetcher_url_selector)
+    elif fetcher_number==2:
+        url_list = url_fetcher2(fetcher_url, fetcher_title_selector, fetcher_url_selector)
+
     url_count = len(url_list)
     print(f'Collected {url_count} links')
     # for url_data in url_list:
